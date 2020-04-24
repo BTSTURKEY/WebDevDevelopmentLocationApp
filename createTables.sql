ca34489dc95f1f87028a7af0f5360cdddbb69cd7
BTSTURKEY <paxt24@gmail.com>
2020-04-24 18:19:49 +0100

    Node.js server added, react enviroment created

--- createTables.sql
+++ createTables.sql
@@ -1,6 +1,9 @@
 CREATE TABLE location_data (
 Phone_ID NUMBER(2) PRIMARY KEY,
-Latitude FLOAT(15),
-Longitude FLOAT(15),
-Time_Stamp DATETIME
+Latitude_GPS DOUBLE(15),
+Longitude_GPS DOUBLE(15),
+Time_Stamp_GPS DATETIME,
+Latitude_NET DOUBLE(15),
+Longitude_NET DOUBLE(15),
+Time_Stamp_NET DATETIME
 );
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
